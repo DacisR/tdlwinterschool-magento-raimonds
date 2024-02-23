38a5f454713ef5f8c3c762f12b731f985b46a6cb
Raimonds Dacis <dacisraimonds@gmail.com>
2024-02-23 15:25:36 +0200

    Fixed incorrect password

--- features/test-data/users.js
+++ features/test-data/users.js
@@ -1,7 +1,7 @@
 export const users = {
     Test: {
         email: "roiquittigrauyei-9048@yopmail.com",
-        password: "qwert123!",
+        password: "qwerty123!",
         firstName: "Test",
         lastName: "Test"
     }
